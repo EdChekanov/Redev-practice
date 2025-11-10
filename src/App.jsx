51aaf1837740fc91abb3575763cc880ff82a47dd
Ed_Chekanov <kide.da@gmail.com>
2025-11-10 19:14:34 +0300

    add Context1 task

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,25 @@ import TaskList from './components/state3/TaskList';
 import Cart from './components/state3/Cart';
 
 import './App.css';
+import Toggle from './components/context/toggle';
+import { ThemeContext, themes } from './contexts/ThemeContext';
+
+Toggle;
 
 function App() {
   return (
     <>
+      <ThemeContext.Consumer>
+        {({ theme, setTheme }) => (
+          <Toggle
+            onChange={() => {
+              if (theme === themes.light) setTheme(themes.dark);
+              if (theme === themes.dark) setTheme(themes.light);
+            }}
+            value={theme === themes.dark}
+          />
+        )}
+      </ThemeContext.Consumer>
       <UserProfile />
       <TaskList />
       <Cart />
